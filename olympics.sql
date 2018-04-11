b831e613cafe4e243f3edec455b7f5ae3b4369f9
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-04-11 11:41:25 +0100

    Fixed scheme and povoar.sql

--- olympics.sql
+++ olympics.sql
@@ -9,64 +9,65 @@ BEGIN TRANSACTION;
 -- Table: Atleta
 DROP TABLE IF EXISTS Atleta;
 CREATE TABLE Atleta (
-        ID INT PRIMARY KEY, 
-        Nome STRING, 
-        DataDeNascimento STRING, 
-        Sexo CHAR CHECK (Sexo = 'F' || Sexo = 'M'), 
-        Peso DOUBLE, 
-        Altura DOUBLE, 
+        ID INT PRIMARY KEY,
+        Nome STRING,
+        DataDeNascimento STRING,
+        Sexo CHAR CHECK (Sexo = 'F' OR Sexo = 'M'),
+        Peso REAL,
+        Altura REAL,
         SiglaPais STRING REFERENCES Pais (Sigla) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- Table: Desporto
 DROP TABLE IF EXISTS Desporto;
 CREATE TABLE Desporto (
-        ID INT PRIMARY KEY, 
+        ID INT PRIMARY KEY,
         Nome STRING UNIQUE
 );
 
 -- Table: EdicaoAtletaEvento
 DROP TABLE IF EXISTS EdicaoAtletaEvento;
 CREATE TABLE EdicaoAtletaEvento (
-        Ano INT REFERENCES EdicaoDosJogos ON DELETE SET NULL ON UPDATE CASCADE, 
-        IdAtleta INT REFERENCES Atleta (ID) ON DELETE SET NULL ON UPDATE CASCADE, 
-        IdEvento INT REFERENCES Evento (ID) ON DELETE SET NULL ON UPDATE CASCADE, 
-        Posicao INT, 
-        Valor DOUBLE, 
+        Ano INT REFERENCES EdicaoDosJogos ON DELETE SET NULL ON UPDATE CASCADE,
+        IdAtleta INT REFERENCES Atleta (ID) ON DELETE SET NULL ON UPDATE CASCADE,
+        IdEvento INT REFERENCES Evento (ID) ON DELETE SET NULL ON UPDATE CASCADE,
+        Posicao INT,
+        Valor REAL,
         PRIMARY KEY (Ano, IdAtleta, IdEvento)
 );
 
 -- Table: EdicaoDosJogos
 DROP TABLE IF EXISTS EdicaoDosJogos;
 CREATE TABLE EdicaoDosJogos (
-        Ano INT PRIMARY KEY, 
-        Cidade STRING, 
-        IdTipoDeJogos INT REFERENCES TipoDeJogos (ID) ON DELETE SET NULL ON UPDATE CASCADE, 
+        Ano INT PRIMARY KEY,
+        Cidade STRING,
+        IdTipoDeJogos INT REFERENCES TipoDeJogos (ID) ON DELETE SET NULL ON UPDATE CASCADE,
         SiglaPais STRING REFERENCES Pais (Sigla) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- Table: Evento
 DROP TABLE IF EXISTS Evento;
 CREATE TABLE Evento (
-        ID INT PRIMARY KEY, 
-        Data STRING, 
-        IdTipoDeEvento INT REFERENCES TipoDeEvento (ID) ON DELETE SET NULL ON UPDATE CASCADE, 
-        IdModalidade INT REFERENCES Modalidade (ID) ON DELETE SET NULL ON UPDATE CASCADE, 
-        Sexo CHAR CHECK (Sexo = 'F' || Sexo = 'M')
+        ID INT PRIMARY KEY,
+        Data STRING,
+        IdTipoDeEvento INT REFERENCES TipoDeEvento (ID) ON DELETE SET NULL ON UPDATE CASCADE,
+        IdModalidade INT REFERENCES Modalidade (ID) ON DELETE SET NULL ON UPDATE CASCADE
+
 );
 
 -- Table: Modalidade
 DROP TABLE IF EXISTS Modalidade;
 CREATE TABLE Modalidade (
-        ID INT PRIMARY KEY, 
-        Nome STRING UNIQUE, 
-        IdDesporto INT REFERENCES Desporto (ID) ON DELETE SET NULL ON UPDATE CASCADE UNIQUE
+        ID INT PRIMARY KEY,
+        Nome STRING,
+        IdDesporto INT REFERENCES Desporto (ID) ON DELETE SET NULL ON UPDATE CASCADE,
+        Sexo CHAR CHECK (Sexo = "F" OR Sexo = "M")
 );
 
 -- Table: Pais
 DROP TABLE IF EXISTS Pais;
 CREATE TABLE Pais (
-        Sigla STRING PRIMARY KEY, 
+        Sigla STRING PRIMARY KEY,
         Nome STRING UNIQUE
 );
 
@@ -74,7 +75,7 @@ CREATE TABLE Pais (
 DROP TABLE IF EXISTS Recorde;
 CREATE TABLE Recorde (
         ID INT PRIMARY KEY,
-        Valor DOUBLE,
+        Valor REAL,
         Data STRING,
         IdAtleta INT REFERENCES Atleta (ID) ON DELETE SET NULL ON UPDATE CASCADE,
         IdTipoDeRecorde INT REFERENCES TipoDeRecord (ID) ON DELETE SET NULL ON UPDATE CASCADE,
@@ -84,14 +85,14 @@ CREATE TABLE Recorde (
 -- Table: TipoDeEvento
 DROP TABLE IF EXISTS TipoDeEvento;
 CREATE TABLE TipoDeEvento (
-        ID INT PRIMARY KEY, 
+        ID INT PRIMARY KEY,
         Nome STRING UNIQUE
 );
 
 -- Table: TipoDeJogos
 DROP TABLE IF EXISTS TipoDeJogos;
 CREATE TABLE TipoDeJogos (
-        ID INT PRIMARY KEY, 
+        ID INT PRIMARY KEY,
         Tipo INT UNIQUE
 );
 
