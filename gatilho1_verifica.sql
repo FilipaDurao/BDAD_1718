2e44e51fe2eaa7d600cdf97e02cacd07813fdac7
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-05-20 21:46:13 +0100

    Incluir os headers nas verificações

--- gatilho1_verifica.sql
+++ gatilho1_verifica.sql
@@ -1,1 +1,4 @@
+.mode columns
+.headers on
+.nullvalue NULL
 INSERT INTO EdicaoAtletaEvento(Posicao, Valor, Ano, IdAtleta, IdEvento) VALUES (1, 9.81, 2016, 579416064,2);
--- gatilho2_verifica.sql
+++ gatilho2_verifica.sql
@@ -1,3 +1,6 @@
+.mode columns
+.headers on
+.nullvalue NULL
 SELECT * FROM Recorde WHERE IdModalidade = 0;
 INSERT INTO EdicaoAtletaEvento(Posicao, Valor, Ano, IdAtleta, IdEvento) VALUES (9, 9.60, 2016, 1,0);
 SELECT * FROM Recorde WHERE IdModalidade = 0;
