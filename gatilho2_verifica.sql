7d4a8ca15487571e116deb92d67200abe591b1e0
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-05-21 09:30:38 +0100

    Modifies the verification of the 2º trigger not trigger the 3rd trigger

--- gatilho2_verifica.sql
+++ gatilho2_verifica.sql
@@ -1,8 +1,9 @@
 .mode columns
 .headers on
 .nullvalue NULL
+INSERT INTO Evento(Id, Data, IdTipoDeEvento, IdModalidade) VALUES (18, "15/8/2016" , 1, 0);
 SELECT * FROM Recorde WHERE IdModalidade = 0;
-INSERT INTO EdicaoAtletaEvento(Posicao, Valor, Ano, IdAtleta, IdEvento) VALUES (9, 9.60, 2016, 1,0);
+INSERT INTO EdicaoAtletaEvento(Posicao, Valor, Ano, IdAtleta, IdEvento) VALUES (2, 9.60, 2016, 1,18);
 SELECT * FROM Recorde WHERE IdModalidade = 0;
-INSERT INTO EdicaoAtletaEvento(Posicao, Valor, Ano, IdAtleta, IdEvento) VALUES (9, 9.3, 2016, 2,0);
+INSERT INTO EdicaoAtletaEvento(Posicao, Valor, Ano, IdAtleta, IdEvento) VALUES (1, 9.3, 2016, 2,18);
 SELECT * FROM Recorde WHERE IdModalidade = 0;
