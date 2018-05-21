9220c7835a9bc42834da4f57b751dc7aba4ede50
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-05-21 09:29:52 +0100

    3º gatilho
    Garante que valores menores estão numa melhor posição

---
+++ gatilho3_adiciona.sql
@@ -0,0 +1,11 @@
+CREATE TRIGGER checkPosition
+BEFORE
+INSERT ON EdicaoAtletaEvento WHEN EXISTS
+  (SELECT *
+   FROM EdicaoAtletaEvento
+   WHERE IdEvento = New.IdEvento
+     AND Ano = New.Ano
+     AND Posicao < New.Posicao
+     AND Valor > New.Valor) BEGIN
+SELECT RAISE
+  (ROLLBACK, "Invalid position at the event."); END
---
+++ gatilho3_verifica.sql
@@ -0,0 +1,1 @@
+INSERT INTO EdicaoAtletaEvento(Posicao, Valor, Ano, IdAtleta, IdEvento) VALUES (9, 9.60, 2016, 1,0);
