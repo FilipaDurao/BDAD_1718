fa04b2c3198a634c69afc8bf8bea48bd87cd52fe
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-05-23 09:14:15 +0100

    Atualização do 3º trigger para considerar posições abaixo

--- gatilho3_adiciona.sql
+++ gatilho3_adiciona.sql
@@ -1,11 +1,13 @@
 CREATE TRIGGER checkPosition
 BEFORE
-INSERT ON EdicaoAtletaEvento WHEN EXISTS
-  (SELECT *
-   FROM EdicaoAtletaEvento
-   WHERE IdEvento = New.IdEvento
-     AND Ano = New.Ano
-     AND Posicao < New.Posicao
-     AND Valor > New.Valor) BEGIN
-SELECT RAISE
-  (ROLLBACK, "Invalid position at the event."); END
+INSERT ON EdicaoAtletaEvento WHEN EXISTS (
+SELECT *
+FROM EdicaoAtletaEvento
+WHERE IdEvento = New.IdEvento
+  AND Ano = New.Ano
+  AND ( (Posicao < New.Posicao
+         AND Valor > New.Valor)
+  OR ((Posicao > New.Posicao
+       AND Valor < New.Valor)))) BEGIN
+  SELECT RAISE
+    (ROLLBACK, "Invalid position at the event."); END
